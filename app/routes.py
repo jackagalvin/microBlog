351f36b315c7c88eb93d1262eee12c424c9a7b99
Jack G <jackagalvin@gmail.com>
2020-01-21 21:19:45 +0000

    Added templating

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello Jack"
+    user = {"username":"Jack"}
+    posts = [
+        {"author":{"username":"John"},
+        "message":"Man it's cold today"},
+        {"author":{"username":"Mark"},
+        "message":"Can we be done with this weather please"}
+    ]
+    return render_template("index.html",title="Home",user=user,posts=posts)
